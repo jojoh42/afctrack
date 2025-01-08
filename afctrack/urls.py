f291272ad9295372fb2e14f5f55fb04e74408d3b
Jesper Johannsen <jjfj.johannsen@gmx.de>
2025-01-08 15:32:50 +0100

    forgot the app_name

--- afctrack/urls.py
+++ afctrack/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'afctrack'  # Add this line to specify the app name
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('doctrine_amount/', views.doctrine_amount, name='doctrine_amount'),
