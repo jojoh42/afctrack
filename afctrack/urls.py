889b0297b7304747d6ec5678fdc840c593c37dda
Jesper Johannsen <jjfj.johannsen@gmx.de>
2025-01-08 15:22:22 +0100

    adding nave and new .html

--- afctrack/urls.py
+++ afctrack/urls.py
@@ -3,11 +3,13 @@
 # Django
 from django.urls import path
 
-# AA afctrack App
 from afctrack import views
 
-app_name: str = "afctrack"
+app_name = "afctrack"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("fleet_counts/", views.fleet_counts, name="fleet_counts"),
+    path("about/", views.about, name="about"),
+    path("doctrine_amount/", views.doctrine_amount, name="doctrine_amount"),  # Add this line
 ]
