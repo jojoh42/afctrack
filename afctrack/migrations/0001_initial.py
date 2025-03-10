cbcc80a201478a33c02aa5dc3682c19282d1e73d
Jesper Johannsen <jjfj.johannsen@gmx.de>
2025-03-10 15:33:15 +0100

    maybe i forgot one area or so

--- afctrack/migrations/0001_initial.py
+++ afctrack/migrations/0001_initial.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
                 ),
             ],
             options={
-                "permissions": (("basic_access", "Can access this app"),),
+                "permissions": (("basic_access", "Can access this app"), ("full_access", "Can access all features")),
                 "managed": False,
                 "default_permissions": (),
             },
