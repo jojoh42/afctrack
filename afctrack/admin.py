3381947d3cc867a25d209f31ec0e6d7ac17d49b5
jojoh42 <helol@gmx.de>
2026-03-04 19:06:21 +0100

    Remove unused SingletonModelAdmin import and registration for AfcTrackSettings

--- afctrack/admin.py
+++ afctrack/admin.py
@@ -1,12 +1,4 @@
 """Admin models"""
 
-# Third Party
-from solo.admin import SingletonModelAdmin  # type: ignore
-
 # Django
 from django.contrib import admin  # noqa: F401
-from .models import AfcTrackSettings
-
-admin.site.register(AfcTrackSettings, SingletonModelAdmin)
-
-# Register your models here.
